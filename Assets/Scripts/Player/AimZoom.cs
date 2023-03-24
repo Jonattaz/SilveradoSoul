28d2f9fed393b962391f0019d94783dd215d0414
Jonattaz <jonatas.silva159@etec.sp.gov.br>
2023-03-24 15:54:05 -0300

    Zoom

---
+++ Assets/Scripts/Player/AimZoom.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimZoom : MonoBehaviour
+{
+    [SerializeField] private int zoom = 20;
+    [SerializeField] private int normal = 60;
+    [SerializeField] private float smooth = 5;
+    private bool isZoomed = false;
+    private bool notZoomed = true;
+
+    void Update()
+    {
+        ZoomController();
+    }
+
+    private void ZoomController(){
+        if (Input.GetButtonDown("Fire2"))
+        {
+            isZoomed = !isZoomed;
+        }
+
+        if (isZoomed)
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView,
+                zoom, Time.deltaTime * smooth);
+        }
+
+        if (Input.GetButtonUp("Fire2"))
+        {
+            isZoomed = !notZoomed;
+        }
+
+        if (notZoomed)
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView,
+                normal, Time.deltaTime * smooth);
+        }
+    }
+
+}
