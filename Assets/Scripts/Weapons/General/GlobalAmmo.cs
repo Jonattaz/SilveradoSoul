d5e867f99eb22401e9587fe115f1a9bff12ca109
Jonattaz <jonatas.silva159@etec.sp.gov.br>
2023-05-22 11:14:05 -0300

    - UI part 1

--- Assets/Scripts/Weapons/General/GlobalAmmo.cs
+++ Assets/Scripts/Weapons/General/GlobalAmmo.cs
@@ -8,10 +8,13 @@ public class GlobalAmmo : MonoBehaviour
     public static int ammo;
     [SerializeField]
     private GameObject ammoDisplay;
+    [SerializeField]
+    private GameObject ammoDisplay2;
 
     // Update is called once per frame
     void Update(){
         ammoDisplay.GetComponent<Text>().text = "" + ammo;
+        ammoDisplay2.GetComponent<Text>().text = "" + ammo;
         
     }
 }
